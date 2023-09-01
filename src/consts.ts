fda9c0c0150265305fa45fbefb2cdac2dfaa9bec
Agustín Bravo Pérez <anbravoperez@gmail.com>
2023-09-01 14:55:38 -0300

    archivos iniciales, prototipo de landing page

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "🧇 Waffre";
+export const SITE_DESCRIPTION =
+  "La Wiki de Apuntes de la Facultad Regional Resistencia de la UTN, un proyecto estudiantil colaborativo.";
+export const BUILD_VERSION = "0.0.1";
